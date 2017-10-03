8967e29d619a756e1b1dc675e6e9e0b9431c23a6
alee-altiscale <alee@altiscale.com>
2017-10-03 12:44:18 -0500

    PERF-58 Divide and conquer tpcds on smaller cluster, Auto-gen on Scala objects and Scala REPL shell code for automation

---
+++ src/main/resources/TPCDS_2_4_template_Queries.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2015 Databricks Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.databricks.spark.sql.perf.tpcds
+
+import org.apache.commons.io.IOUtils
+
+import com.databricks.spark.sql.perf.{Benchmark, ExecutionMode, Query}
+
+/**
+ * This implements the official TPCDS v2.4 queries with only cosmetic modifications.
+ */
+trait Tpcds_2_4_REPLACEME_Queries extends Benchmark {
+
+  import ExecutionMode._
+
+  private val queryNames = Seq(
+    LIST_TO_UPDATE
+  )
+
+  val tpcds2_4_REPLACEMEQueries = queryNames.map { queryName =>
+    val queryContent: String = IOUtils.toString(
+      getClass().getClassLoader().getResourceAsStream(s"tpcds_2_4_REPLACEME/$queryName.sql"))
+    Query(queryName + "-v2.4", queryContent, description = "TPCDS 2.4 Query",
+      executionMode = CollectResults)
+  }
+
+  val tpcds2_4_REPLACEMEQueriesMap = tpcds2_4_REPLACEMEQueries.map(q => q.name.split("-").get(0) -> q).toMap
+}
---
+++ tpcds_split_queries.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+curr_dir=$(dirname $0)
+curr_dir=$(cd $curr_dir; pwd)
+
+tpcds_resource_dir=$curr_dir/src/main/resources
+tpcds_all_queries=$tpcds_resource_dir/tpcds_2_4
+tpcds_query_dst=$curr_dir/src/main/scala/com/databricks/spark/sql/perf/tpcds
+tpcds_query_template=$tpcds_resource_dir/TPCDS_2_4_template_Queries.scala
+tpcds_scala_template=$tpcds_resource_dir/tpcds_repl_2_4_template.scala
+
+function usage {
+cat << EOF
+  This script splits the query from the $tpcds_all_queries directory into
+    sub-directories, and generate the code as Scala objects. It also produce the
+    scala file with prefix tpcds_2_4_pXX.scala for Spark REPL shell to execute in the
+    current directory. The results are stored in each sub-directory on HDFS based on the -o option.
+    You can specify the target database to query via the -d option.
+
+  Usage: 
+	  $0 -o hdfs_output_dir -d tpcds_database_name
+
+  Example:
+	  $0 -o /tmp/tpcds_partial_results -d tpcds_bin_partitioned_parquet_10000
+EOF
+  exit 1
+}
+
+while getopts hd:o: opt
+do
+  case "$opt" in
+    d) database_name="$OPTARG";;
+    o) output_hdfs_dir="$OPTARG";;
+    h) usage; exit 1;;
+    ?) usage; exit 1;;
+  esac
+done
+
+use_database=$database_name
+store_qry_result=$output_hdfs_dir
+
+for f in `seq -w 1 20`
+do
+  mkdir -p $tpcds_resource_dir/tpcds_2_4_p${f}
+done
+
+find $tpcds_all_queries -type f | sort | split -l 6 - xxxtpcds_
+
+i=1
+for f in `find . -type f -name "xxxtpcds_*" | sort`
+do
+  idx_code=
+  qlist_tmp=""
+  for ff in `cat $f`
+  do
+    bname=`echo $(basename $ff) | cut -d. -f1`
+    qlist_tmp="$qlist_tmp,\"$bname\""
+    if [ $i -le 9 ] ; then
+      cp $ff $tpcds_resource_dir/tpcds_2_4_p0${i}/
+      idx_code="p0${i}"
+    else
+      cp $ff $tpcds_resource_dir/tpcds_2_4_p${i}/
+      idx_code="p${i}"
+    fi
+  done
+  # Generate tpcds code
+  qlist=`echo $qlist_tmp | sed "s/^,//"`
+  cat $tpcds_query_template | \
+    sed "s/REPLACEME/$idx_code/g" | \
+    sed "s/LIST_TO_UPDATE/$qlist/g" > \
+    $tpcds_query_dst/TPCDS_2_4_${idx_code}_Queries.scala.tmp
+  cat $tpcds_query_dst/TPCDS_2_4_${idx_code}_Queries.scala.tmp | \
+    sed "s/LIST_TO_UPDATE/$qlist/g" > \
+    $tpcds_query_dst/TPCDS_2_4_${idx_code}_Queries.scala
+  rm -f $tpcds_query_dst/TPCDS_2_4_${idx_code}_Queries.scala.tmp
+  echo "ok - generated query object tpcds2_4_${idx_code}Queries in code, use \"val queries = tpcds.tpcds2_4_${idx_code}Queries\" in your Scala shell"
+  # Generate Scala REPL code for execution and automation
+  cat $tpcds_scala_template | \
+    sed "s/QUERYIDX/$idx_code/g" | \
+    sed "s:HDFSDEST:$store_qry_result:g" | \
+    sed "s/TARGETDATABASE/$use_database/g" > \
+		$curr_dir/tpcds_2_4_${idx_code}.scala
+  let "i++"
+done
+
+rm -f xxxtpcds_*
+exit 0
