c0d20b6b14319132460c6ae72cd22881a5a66b11
Sarah Victor <sarah@altiscale.com>
2017-11-20 10:56:58 -0800

    q33.sql

--- src/main/resources/tpcds_2_4/q33.sql
+++ src/main/resources/tpcds_2_4/q33.sql
@@ -1,49 +1,52 @@
 --q33.sql--
 
  with ss as (
-    select
-        i_manufact_id,sum(ss_ext_sales_price) total_sales
-    from
- 	      store_sales, date_dim, customer_address, item
-    where
-        i_manufact_id in (select i_manufact_id
-                          from item
-                          where i_category in ('Electronics'))
-                            and ss_item_sk = i_item_sk
-                            and ss_sold_date_sk = d_date_sk
-                            and d_year = 1998
-                            and d_moy = 5
-                            and ss_addr_sk = ca_address_sk
-                            and ca_gmt_offset = -5
-                          group by i_manufact_id), cs as
-         (select i_manufact_id, sum(cs_ext_sales_price) total_sales
-          from catalog_sales, date_dim, customer_address, item
-          where
-            i_manufact_id in (
-                select i_manufact_id from item
-                where
-                    i_category in ('Electronics'))
-                    and cs_item_sk = i_item_sk
-                    and cs_sold_date_sk = d_date_sk
-                    and d_year = 1998
-                    and d_moy = 5
-                    and cs_bill_addr_sk = ca_address_sk
-                    and ca_gmt_offset = -5
-                group by i_manufact_id),
- ws as (
- select i_manufact_id,sum(ws_ext_sales_price) total_sales
- from
- 	  web_sales, date_dim, customer_address, item
- where
-    i_manufact_id in (select i_manufact_id from item
-                      where i_category in ('Electronics'))
-                          and ws_item_sk = i_item_sk
-                          and ws_sold_date_sk = d_date_sk
-                          and d_year = 1998
-                          and d_moy = 5
-                          and ws_bill_addr_sk = ca_address_sk
-                          and ca_gmt_offset = -5
-                      group by i_manufact_id)
+   select i1.i_manufact_id, sum(ss_ext_sales_price) total_sales
+     from store_sales JOIN date_dim ON
+       ss_sold_date_sk = d_date_sk
+     JOIN item i1 ON 
+       ss_item_sk = i1.i_item_sk
+     JOIN customer_address ON
+       ss_addr_sk = ca_address_sk
+     LEFT SEMI JOIN item i2 ON
+       (i1.i_manufact_id = i2.i_manufact_id AND
+       i2.i_category = 'Electronics')
+     WHERE
+       d_year = 1998
+       and d_moy = 5
+       and ca_gmt_offset = -5
+    GROUP BY i1.i_manufact_id), 
+
+  cs as (SELECT i_manufact_id, sum(cs_ext_sales_price) total_sales
+    FROM catalog_sales JOIN date_dim ON
+      cs_sold_date_sk = d_date_sk
+    JOIN item i1 ON
+      cs_item_sk = i1.i_item_sk
+    JOIN customer_address ON 
+      cs_bill_addr_sk = ca_address_sk
+    LEFT SEMI JOIN item i2 ON
+      (i1.i_manufact_id = i2.i_manufact_id AND
+       i2.i_category = 'Electronics')
+    WHERE d_year = 1998
+      and d_moy = 5
+      and ca_gmt_offset = -5
+      group by i1.i_manufact_id),
+
+ ws as ( select i1.i_manufact_id,sum(ws_ext_sales_price) total_sales
+   FROM web_sales JOIN date_dim ON 
+     ws_sold_date_sk = d_date_sk
+   JOIN customer_address ON
+     ws_bill_addr_sk = ca_address_sk
+   JOIN item i1 ON
+     ws_item_sk = i1.i_item_sk
+   LEFT SEMI JOIN item i2 ON
+     (i1.i_manufact_id = i2.i_manufact_id AND
+      i2.i_category = 'Electronics')
+   WHERE d_year = 1998
+      and d_moy = 5
+      and ca_gmt_offset = -5
+      group by i1.i_manufact_id)
+
  select i_manufact_id ,sum(total_sales) total_sales
  from  (select * from ss
         union all
