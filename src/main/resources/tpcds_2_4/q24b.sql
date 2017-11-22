fd29ae6262581b7c17aa6d6cd125c976465d21be
Max Ziff <ziff@altiscale.com>
2017-11-22 19:27:41 +0000

    a few more queries from max

--- src/main/resources/tpcds_2_4/q24b.sql
+++ src/main/resources/tpcds_2_4/q24b.sql
@@ -13,10 +13,13 @@
    and s_zip = ca_zip
    and s_market_id = 8
  group by c_last_name, c_first_name, s_store_name, ca_state, s_state,
-          i_color, i_current_price, i_manager_id, i_units, i_size)
+          i_color, i_current_price, i_manager_id, i_units, i_size),
+avgpaid as
+(select 0.05*avg(netpaid) as av from ssales)
  select c_last_name, c_first_name, s_store_name, sum(netpaid) paid
  from ssales
+ left join avgpaid
  where i_color = 'chiffon'
- group by c_last_name, c_first_name, s_store_name
- having sum(netpaid) > (select 0.05*avg(netpaid) from ssales)
+ group by c_last_name, c_first_name, s_store_name, avgpaid.av
+ having sum(netpaid) > 0.05*avgpaid.av
             
